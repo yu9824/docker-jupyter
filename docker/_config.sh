dc2ab91bb2528a6badd21ee0b089a581eda0650b
yu9824 <58211916+yu9824@users.noreply.github.com>
2022-03-12 22:11:27 +0900

    Initial commit.

---
+++ docker/_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# variable
+SCRIPT_DIR=$(cd $(dirname $0)/..; pwd)
+DIR_NAME=$(basename $SCRIPT_DIR)
+IMAGE_NAME="my_img"
+IMAGE_VERSION=1.0.0
+USER_NAME=anaconda
+DOCKER_USER_ID=yu9824
